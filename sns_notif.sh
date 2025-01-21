6534f1abe976cec1c4c2aa76474a8888b9828516
mkhosi-cpu <mkhosisiyabonga@gmail.com>
2025-01-21 20:24:18 +0200

    SNS update

--- sns_notif.sh
+++ sns_notif.sh
@@ -2,6 +2,6 @@
 
 aws sns create-topic --name mission-control-alerts
 aws sns subscribe \
-    --topic-arn arn:aws:sns:us-east-1:123456789012:mission-control-alerts \
+    --topic-arn arn:aws:sns:us-east-1:724772067672:mission-control-alerts \
     --protocol email \
     --notification-endpoint KelezaDevOps@keleza.site
